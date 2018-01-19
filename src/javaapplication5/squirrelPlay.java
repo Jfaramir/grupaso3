69c5902692819e226d13be44c7d9db9b1713e5fb
xp <xp@CETYSDAM029>
2018-01-19 18:58:38 +0100

    joak

---
+++ src/javaapplication5/squirrelPlay.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author xp
+ */
+public class squirrelPlay {
+        public boolean squirrelPlay(int temp, boolean isSummer) {
+        if (isSummer && 60 <= temp && temp <= 100) {
+            return true;
+        }
+
+        if (!isSummer && 60 <= temp && temp <= 90) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
